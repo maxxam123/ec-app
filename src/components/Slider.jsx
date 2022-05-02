b3aca5ca8ed3afb0bb1b0cb802e684cab4493c92
max <hennighausenmax@gmail.com>
2022-05-02 19:16:33 +0200

    modified Slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -98,11 +98,11 @@ const Slider = () => {
 
   const handleClick = (direction) => {
     if (direction === 'left') {
-      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : test);
-      // setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+      // setSlideIndex(slideIndex > 0 ? slideIndex - 1 : test);
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
     } else {
-      setSlideIndex(slideIndex < test ? slideIndex + 1 : 0);
-      // setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+      // setSlideIndex(slideIndex < test ? slideIndex + 1 : 0);
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
     }
   };
 
